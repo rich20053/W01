30a9e35c0a8bd937df9da026cd2700dc62d3a1c3
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2024-01-13 13:21:47 -0700

    Initial Version

---
+++ server.js
@@ -0,0 +1,12 @@
+// Ecpress web server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Aubrey Richmond');
+})
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log("Web server is listening at port " + (process.env.port || port));
